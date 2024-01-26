bfb4cca51757b0c7cee7ce31f9ec08b8cb32666b
b2hinkle <b2hinkle@gmail.com>
2024-01-25 21:56:36 -0500

    Add Game Core helpers BoolToCString() and GetWorldNetMode() and overload LexToString() for ENetMode and ENetRole. Also export GCUtils variables and functions.

--- Source/GameCore/Private/GCUtils.cpp
+++ Source/GameCore/Private/GCUtils.cpp
@@ -2,6 +2,44 @@
 
 #include "GCUtils.h"
 
+const TCHAR* LexToString(ENetMode inNetMode)
+{
+    switch (inNetMode)
+    {
+        case ENetMode::NM_Standalone:
+            return TEXT("NM_Standalone");
+        case ENetMode::NM_DedicatedServer:
+            return TEXT("NM_DedicatedServer");
+        case ENetMode::NM_ListenServer:
+            return TEXT("NM_ListenServer");
+        case ENetMode::NM_Client:
+            return TEXT("NM_Client");
+        case ENetMode::NM_MAX:
+            return TEXT("NM_MAX");
+    }
+
+    return TEXT("Unknown");
+}
+
+const TCHAR* LexToString(ENetRole inNetRole)
+{
+    switch (inNetRole)
+    {
+        case ENetRole::ROLE_None:
+            return TEXT("ROLE_None");
+        case ENetRole::ROLE_SimulatedProxy:
+            return TEXT("ROLE_SimulatedProxy");
+        case ENetRole::ROLE_AutonomousProxy:
+            return TEXT("ROLE_AutonomousProxy");
+        case ENetRole::ROLE_Authority:
+            return TEXT("ROLE_Authority");
+        case ENetRole::ROLE_MAX:
+            return TEXT("ROLE_MAX");
+    }
+
+    return TEXT("Unknown");
+}
+
 FString GCUtils::GetUObjectNameSafe(const UObject* inUObject)
 {
     return inUObject ? inUObject->GetName() : TEXT("NULL");
@@ -10,4 +48,19 @@ FString GCUtils::GetUObjectNameSafe(const UObject* inUObject)
 FString GCUtils::GetUObjectFullNameSafe(const UObject* inUObject, const UObject* StopOuter, EObjectFullNameFlags Flags)
 {
     return inUObject ? inUObject->GetFullName(StopOuter, Flags) : TEXT("NULL");
+}
+
+const TCHAR* GCUtils::BoolToCString(const bool inBool)
+{
+    return inBool ? TEXT("True") : TEXT("False");
+}
+
+const TCHAR* GCUtils::GetWorldNetMode(const UWorld* inWorld)
+{
+    if (!inWorld)
+    {
+        return TEXT("Null World");
+    }
+
+    return LexToString(inWorld->GetNetMode());
 }
--- Source/GameCore/Public/GCUtils.h
+++ Source/GameCore/Public/GCUtils.h
@@ -4,11 +4,31 @@
 
 #include "CoreMinimal.h"
 
+/**
+ * Returns the string representation of the specified ENetMode value.
+ *
+ * @param inNetMode The string representation of ENetMode.
+ * @return A CString representation of the net mode.
+ */
+GAMECORE_API const TCHAR* LexToString(ENetMode inNetMode);
+
+/**
+ * Returns the string representation of the specified ENetRole value.
+ *
+ * @param inNetRole The string representation of ENetRole.
+ * @return A CString representation of the net role.
+ */
+GAMECORE_API const TCHAR* LexToString(ENetRole inNetRole);
+
 namespace GCUtils
 {
-    inline constexpr const TCHAR* CStringNull = TEXT("NULL");
+    GAMECORE_API inline constexpr const TCHAR* CStringNull = TEXT("NULL");
 
-    FString GetUObjectNameSafe(const UObject* inUObject);
+    GAMECORE_API FString GetUObjectNameSafe(const UObject* inUObject);
 
-    FString GetUObjectFullNameSafe(const UObject* inUObject, const UObject* StopOuter=NULL, EObjectFullNameFlags Flags = EObjectFullNameFlags::None);
+    GAMECORE_API FString GetUObjectFullNameSafe(const UObject* inUObject, const UObject* StopOuter = NULL, EObjectFullNameFlags Flags = EObjectFullNameFlags::None);
+
+    GAMECORE_API const TCHAR* BoolToCString(const bool inBool);
+
+    GAMECORE_API const TCHAR* GetWorldNetMode(const UWorld* inWorld);
 }
