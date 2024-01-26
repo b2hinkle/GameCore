e6c2205fa811482582c348e67d80d457c4e62f71
Christian Hinkle <christianhinko@gmail.com>
2024-01-25 21:19:24 -0500

    Make basic GC_LOG and GC_CLOG macros.

--- Source/GameCore/Public/GCLogUtils.h
+++ Source/GameCore/Public/GCLogUtils.h
@@ -4,6 +4,15 @@
 
 #include "CoreMinimal.h"
 
+// Note: Using `##` before `__VA_ARGS__` is not officially supported by the standard but is supported by
+// major compilers. UE_LOG() uses though so we will do the same.
+
+// TODO: Output netmode and as much info as we can in these logs.
+
+#define GC_LOG(categoryName, verbosity, format, ...) UE_LOG(categoryName, verbosity, TEXT("[%s] ") format, ANSI_TO_TCHAR(__func__), ##__VA_ARGS__)
+
+#define GC_CLOG(condition, categoryName, verbosity, format, ...) UE_CLOG(condition, categoryName, verbosity, TEXT("[%s] ") format, ANSI_TO_TCHAR(__func__), ##__VA_ARGS__)
+
 namespace GCLogUtils
 {
     inline constexpr const TCHAR* CStringNull = TEXT("NULL");
