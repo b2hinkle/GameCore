fe898c09042c9aeba0500f977f1ff0fed713dab4
Christian Hinkle <christianhinko@gmail.com>
2024-02-11 01:44:22 -0500

    Change non-standard `##__VA_ARGS__` to `__VA_OPT(,)__ __VA_ARGS__`. C++20 feature.

--- Source/GameCore/Public/GCUtils.h
+++ Source/GameCore/Public/GCUtils.h
@@ -20,17 +20,17 @@
 /**
  * If you can't get ahold of a UObject, you may need to use this.
  */
-#define GC_LOG_NO_CONTEXT(categoryName, verbosity, format, ...) UE_LOG(categoryName, verbosity, TEXT("[%s] ") format, ANSI_TO_TCHAR(__func__), ##__VA_ARGS__)
+#define GC_LOG_NO_CONTEXT(categoryName, verbosity, format, ...) UE_LOG(categoryName, verbosity, TEXT("[%s] ") format, ANSI_TO_TCHAR(__func__) __VA_OPT__(,) __VA_ARGS__)
 
 /**
  * Conditional log version of GC_LOG_NO_CONTEXT().
  */
-#define GC_CLOG_NO_CONTEXT(condition, categoryName, verbosity, format, ...) UE_CLOG(condition, categoryName, verbosity, TEXT("[%s] ") format, ANSI_TO_TCHAR(__func__), ##__VA_ARGS__)
+#define GC_CLOG_NO_CONTEXT(condition, categoryName, verbosity, format, ...) UE_CLOG(condition, categoryName, verbosity, TEXT("[%s] ") format, ANSI_TO_TCHAR(__func__) __VA_OPT__(,) __VA_ARGS__)
 
 /**
  * Format cstring to print info of a context object.
  */
-#define GC_CONTEXT_OBJECT_FORMAT_CSTRING TEXT("[Context Object: %s] [Context World NetMode: %s] ")
+#define GC_CONTEXT_OBJECT_FORMAT_CSTRING TEXT("[Context Object: %s] [Context World NetMode: %s]")
 
 /**
  * Variadic arguments that correspond with GC_CONTEXT_OBJECT_FORMAT_CSTRING.
@@ -40,12 +40,12 @@
 /**
  * @param contextObject - UObject to get the world from so that we can output its net mode. May be a UWorld.
  */
-#define GC_LOG(contextObject, categoryName, verbosity, format, ...) GC_LOG_NO_CONTEXT(categoryName, verbosity, GC_CONTEXT_OBJECT_FORMAT_CSTRING format, GC_CONTEXT_OBJECT_FORMAT_ARGS(contextObject), ##__VA_ARGS__)
+#define GC_LOG(contextObject, categoryName, verbosity, format, ...) GC_LOG_NO_CONTEXT(categoryName, verbosity, GC_CONTEXT_OBJECT_FORMAT_CSTRING TEXT(" ") format, GC_CONTEXT_OBJECT_FORMAT_ARGS(contextObject) __VA_OPT__(,) __VA_ARGS__)
 
 /**
  * Conditional log version of GC_LOG().
  */
-#define GC_CLOG(contextObject, condition, categoryName, verbosity, format, ...) GC_CLOG_NO_CONTEXT(condition, categoryName, verbosity, GC_CONTEXT_OBJECT_FORMAT_CSTRING format, GC_CONTEXT_OBJECT_FORMAT_ARGS(contextObject), ##__VA_ARGS__)
+#define GC_CLOG(contextObject, condition, categoryName, verbosity, format, ...) GC_CLOG_NO_CONTEXT(condition, categoryName, verbosity, GC_CONTEXT_OBJECT_FORMAT_CSTRING TEXT(" ") format, GC_CONTEXT_OBJECT_FORMAT_ARGS(contextObject) __VA_OPT__(,) __VA_ARGS__)
 
 /**
  * Returns the string representation of the specified ENetMode value.
