d218376e9887248fbd008ce37410005d0954f73c
b2hinkle <b2hinkle@gmail.com>
2024-02-11 02:42:18 -0500

    Move log macros to new file "GCUtils_Log.h"

--- Source/GameCore/Public/GCUtils.h
+++ Source/GameCore/Public/GCUtils.h
@@ -18,36 +18,6 @@
 #define GC_CSTRING_NULL TEXT("NULL")
 
 /**
- * If you can't get ahold of a UObject, you may need to use this.
- */
-#define GC_LOG_NO_CONTEXT(categoryName, verbosity, format, ...) UE_LOG(categoryName, verbosity, TEXT("[%s] ") format, ANSI_TO_TCHAR(__func__) __VA_OPT__(,) __VA_ARGS__)
-
-/**
- * Conditional log version of GC_LOG_NO_CONTEXT().
- */
-#define GC_CLOG_NO_CONTEXT(condition, categoryName, verbosity, format, ...) UE_CLOG(condition, categoryName, verbosity, TEXT("[%s] ") format, ANSI_TO_TCHAR(__func__) __VA_OPT__(,) __VA_ARGS__)
-
-/**
- * Format cstring to print info of a context object.
- */
-#define GC_CONTEXT_OBJECT_FORMAT_CSTRING TEXT("[Context Object: %s] [Context World NetMode: %s]")
-
-/**
- * Variadic arguments that correspond with GC_CONTEXT_OBJECT_FORMAT_CSTRING.
- */
-#define GC_CONTEXT_OBJECT_FORMAT_ARGS(contextObject) *GCUtils::GetUObjectNameSafe(contextObject), GCUtils::GetWorldNetModeCString(contextObject)
-
-/**
- * @param contextObject - UObject to get the world from so that we can output its net mode. May be a UWorld.
- */
-#define GC_LOG(contextObject, categoryName, verbosity, format, ...) GC_LOG_NO_CONTEXT(categoryName, verbosity, format TEXT(" ") GC_CONTEXT_OBJECT_FORMAT_CSTRING, __VA_ARGS__ __VA_OPT__(,) GC_CONTEXT_OBJECT_FORMAT_ARGS(contextObject))
-
-/**
- * Conditional log version of GC_LOG().
- */
-#define GC_CLOG(contextObject, condition, categoryName, verbosity, format, ...) GC_CLOG_NO_CONTEXT(condition, categoryName, verbosity, format TEXT(" ") GC_CONTEXT_OBJECT_FORMAT_CSTRING, __VA_ARGS__ __VA_OPT__(,) GC_CONTEXT_OBJECT_FORMAT_ARGS(contextObject))
-
-/**
  * Returns the string representation of the specified ENetMode value.
  *
  * @param inNetMode The string representation of ENetMode.
