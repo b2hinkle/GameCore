e6cc73462beefea62d8efd3a3e6ff5e16325b8d4
b2hinkle <b2hinkle@gmail.com>
2024-02-16 22:52:44 -0500

    Add context object net role as a detail in our context object logs.

--- Source/GameCore/Public/GCUtils_Log.h
+++ Source/GameCore/Public/GCUtils_Log.h
@@ -18,12 +18,12 @@
 /**
  * Format cstring to print info of a context object.
  */
-#define GC_CONTEXT_OBJECT_FORMAT_CSTRING TEXT("[Context Object Arg: `%s`] [Context Object: %s] [Context World NetMode: %s]")
+#define GC_CONTEXT_OBJECT_FORMAT_CSTRING TEXT("[Context Object Arg: `%s`] [Context Object: %s] [Context Object NetMode: %s] [Context Object NetRole: %s]")
 
 /**
  * Variadic arguments that correspond with GC_CONTEXT_OBJECT_FORMAT_CSTRING.
  */
-#define GC_CONTEXT_OBJECT_FORMAT_ARGS(contextObject, contextObjectArgCString) contextObjectArgCString, *GCUtils::GetUObjectNameSafe(contextObject), GCUtils::GetWorldNetModeCString(contextObject)
+#define GC_CONTEXT_OBJECT_FORMAT_ARGS(contextObject, contextObjectArgCString) contextObjectArgCString, *GCUtils::GetUObjectNameSafe(contextObject), GCUtils::GetWorldNetModeCString(contextObject), GCUtils::GetObjectLocalRoleCString(contextObject)
 
 /**
  * @param contextObject - UObject which will be used when our log gathers extra data to help us out. May be a UWorld.
