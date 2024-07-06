4a64c8c4a09b69f799c2173833b1f2bdb83fdd59
b2hinkle <b2hinkle@gmail.com>
2024-07-06 00:04:27 -0400

    [Logging] Improvements to game core logging logic and make its functionality more modular.Remove macros GC_FORMAT_CONTEXT_OBJECT_STRING_LITERAL and GC_FORMAT_CONTEXT_OBJECT_ARGS, and replace them with function GCUtils::GCLog::GetObjectLogInfoCString(). This will allow us to use the object logging outside of our logging (e.g. helpful for assertions with messages).

--- Source/GameCore/Public/GCLog.h
+++ Source/GameCore/Public/GCLog.h
@@ -27,21 +27,41 @@
 #define GC_CLOG_NO_CONTEXT(condition, categoryName, verbosity, format, ...) UE_CLOG(condition, categoryName, verbosity, GC_FORMAT_NO_CONTEXT_STRING_LITERAL TEXT(" ") format, GC_FORMAT_NO_CONTEXT_ARGS __VA_OPT__(,) __VA_ARGS__)
 
 /**
- * Format string to output info about a context object. Corresponds with GC_FORMAT_CONTEXT_OBJECT_ARGS().
+ * Format string for logging output about a context object.
  */
-#define GC_FORMAT_CONTEXT_OBJECT_STRING_LITERAL TEXT("[Context Object Arg: `%s`] [Context Object: %s] [Context Object NetMode: %s] [Context Object NetRole: %s] [Context Object Controller: %s] [Context Object Controller Is Local: %s]")
-
-/**
- * Variadic arguments which corresponds with GC_FORMAT_CONTEXT_OBJECT_STRING_LITERAL.
- */
-#define GC_FORMAT_CONTEXT_OBJECT_ARGS(contextObject, contextObjectArgCString) contextObjectArgCString, GCUtils::String::GetUObjectNameSafe(contextObject).ToString(), GCUtils::String::GetWorldNetModeString(contextObject).GetData(), GCUtils::String::GetObjectLocalRoleString(contextObject).GetData(), GCUtils::String::GetUObjectNameSafe(GCUtils::GetController(contextObject)).ToString(), GCUtils::String::GetIsControllerLocalString(contextObject).GetData()
+#define GC_FORMAT_CONTEXT_OBJECT_STRING_LITERAL TEXT("[Context Object Arg: `%s`]")
 
 /**
  * @param contextObject UObject which will be used when our log gathers extra data to help us out. May be a UWorld.
  */
-#define GC_LOG(contextObject, categoryName, verbosity, format, ...) GC_LOG_NO_CONTEXT(categoryName, verbosity, format TEXT(" ") GC_FORMAT_CONTEXT_OBJECT_STRING_LITERAL, __VA_ARGS__ __VA_OPT__(,) GC_FORMAT_CONTEXT_OBJECT_ARGS(contextObject, TEXT(PREPROCESSOR_TO_STRING(contextObject))))
+#define GC_LOG(contextObject, categoryName, verbosity, format, ...) GC_LOG_NO_CONTEXT(categoryName, verbosity, format TEXT(" ") GC_FORMAT_CONTEXT_OBJECT_STRING_LITERAL TEXT(" %s"), __VA_ARGS__ __VA_OPT__(,) TEXT(PREPROCESSOR_TO_STRING(contextObject)), GCUtils::Log::GetObjectLogInfoString(contextObject).ToString())
 
-/**
- * Conditional log version of GC_LOG().
- */
-#define GC_CLOG(contextObject, condition, categoryName, verbosity, format, ...) GC_CLOG_NO_CONTEXT(condition, categoryName, verbosity, format TEXT(" ") GC_FORMAT_CONTEXT_OBJECT_STRING_LITERAL, __VA_ARGS__ __VA_OPT__(,) GC_FORMAT_CONTEXT_OBJECT_ARGS(contextObject, TEXT(PREPROCESSOR_TO_STRING(contextObject))))
+ /**
+  * Conditional log version of GC_LOG().
+  */
+#define GC_CLOG(contextObject, condition, categoryName, verbosity, format, ...) GC_CLOG_NO_CONTEXT(condition, categoryName, verbosity, format TEXT(" ") GC_FORMAT_CONTEXT_OBJECT_STRING_LITERAL TEXT(" %s"), __VA_ARGS__ __VA_OPT__(,) TEXT(PREPROCESSOR_TO_STRING(contextObject)), GCUtils::Log::GetObjectLogInfoString(contextObject).ToString())
+
+namespace GCUtils::Log
+{
+    /**
+     * Builds a debug string given a UObject.
+     */
+    template <int32 bufferLength = 1024>
+    TStringBuilder<bufferLength> GetObjectLogInfoString(const UObject* object);
+}
+
+template <int32 bufferLength>
+TStringBuilder<bufferLength> GCUtils::Log::GetObjectLogInfoString(const UObject* object)
+{
+    return TStringBuilder<bufferLength>(EInPlace::InPlace,
+        TEXT("[Context Object: "), GCUtils::String::GetUObjectNameSafe(object).ToString(), TEXT(']'),
+        TEXT(' '),
+        TEXT("[Context Object NetMode: "), GCUtils::String::GetWorldNetModeString(object).GetData(), TEXT(']'),
+        TEXT(' '),
+        TEXT("[Context Object NetRole: "), GCUtils::String::GetObjectLocalRoleString(object).GetData(), TEXT(']'),
+        TEXT(' '),
+        TEXT("[Context Object Controller: "), GCUtils::String::GetUObjectNameSafe(GCUtils::GetController(object)).ToString(), TEXT(']'),
+        TEXT(' '),
+        TEXT("[Context Object Controller Is Local: "), GCUtils::String::GetIsControllerLocalString(object).GetData(), TEXT(']')
+    );
+}
