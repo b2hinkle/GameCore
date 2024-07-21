19816369380372563342ae5ffbf33c81f1176f0c
Christian Hinkle <christianhinko@gmail.com>
2024-07-20 21:43:50 -0400

    [String] Fix bad "forwarding" reference code in `TStringBuilderAppender<>`.
    Old, bad code: Constructor was trying to forward the rvalue reference to the functor despite it not being a forwarding reference because `TFunctor` is a class template parameter.
    
    Brake up the constructor into lvalue and rvalue reference overloads and enforce that the class template parameter `TFunctor` is not a reference.
    
    Update `ConstructStringBuilderAppender<>()` to remove the reference when passing in the functor type template argument.

--- Source/GameCore/Public/String/StringBuilderAppender.h
+++ Source/GameCore/Public/String/StringBuilderAppender.h
@@ -26,13 +26,27 @@ namespace GCUtils::String
     {
     public:
 
+        static_assert(TIsReferenceType<TFunctor>::Value == false, "The type template argument of the functor must not be a reference.");
+
+    public:
+
+        /**
+         * @brief Construct from a callback functor.
+         * @param inCallbackFunctor Functor to be called on in the string builder append
+         *                          operator overload.
+         */
+        TStringBuilderAppender(const TFunctor& inCallbackFunctor)
+            : CallbackFunctor(inCallbackFunctor)
+        {
+        }
+
         /**
          * @brief Construct from a callback functor.
          * @param inCallbackFunctor Functor to be called on in the string builder append
          *                          operator overload.
          */
         TStringBuilderAppender(TFunctor&& inCallbackFunctor)
-            : CallbackFunctor(Forward<TFunctor>(inCallbackFunctor))
+            : CallbackFunctor(MoveTemp(inCallbackFunctor))
         {
         }
 
@@ -56,17 +70,20 @@ namespace GCUtils::String
     };
 
     /**
-     * @brief Wrapper function of the `TStringBuilderAppender<>` constructor to provide function
+     * @brief Wrapper function of the `TStringBuilderAppender<>` constructors to provide function
      *        template deduction rules when constructing the template class.
      * @tparam TCharType Type of character in the string builders to support.
-     * @tparam TFunctor Type of the callback functor to store.
-     * @param inCallbackFunctor Functor to be called on in the string builder append
-     *                          operator overload.
+     * @tparam TFunctorRef Reference to the type of the callback functor to store.
+     * @param inCallbackFunctor Forwarding reference to the functor to be called on in the string
+     *                          builder append operator overload.
      * @return A prvalue of the constructed `TStringBuilderAppender<>`.
      */
-    template <class TCharType, class TFunctor>
-    TStringBuilderAppender<TCharType, TFunctor> ConstructStringBuilderAppender(TFunctor&& inCallbackFunctor)
+    template <class TCharType, class TFunctorRef>
+    TStringBuilderAppender<TCharType, typename TRemoveReference<TFunctorRef>::Type> ConstructStringBuilderAppender(TFunctorRef&& inCallbackFunctor)
     {
-        return TStringBuilderAppender<TCharType, TFunctor>(Forward<TFunctor>(inCallbackFunctor));
+        // Note: `TFunctorRef` has a reference baked into it since `TFunctorRef&&` is a forwarding reference. We
+        // make sure to remove that when passing it in as the `TFunctor` argument.
+        return TStringBuilderAppender<TCharType, typename TRemoveReference<TFunctorRef>::Type>(
+            Forward<TFunctorRef>(inCallbackFunctor));
     }
 }
