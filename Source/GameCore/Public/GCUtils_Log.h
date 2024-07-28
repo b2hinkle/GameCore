16a1d2477fb13af31bb4c7a8299e4b23414df4ae
Christian Hinkle <christianhinko@gmail.com>
2024-07-27 20:00:57 -0400

    [Log] Rename contextual log macros with "_UOBJECT" to show they are specific to UObjects.
    Allows us to make more versions of these log macros for other, non-UObject contexts.
    
    Improve documentation too. Format as doxygen.

--- Source/GameCore/Public/GCUtils_Log.h
+++ Source/GameCore/Public/GCUtils_Log.h
@@ -7,44 +7,47 @@
 #include <source_location>
 
 /**
- * Format string to output info about the current context. Corresponds with GC_FORMAT_NO_CONTEXT_ARGS.
+ * @brief Format string literal used in no-context log macros. To output info about the current context.
+ * @note Corresponds with `GC_FORMAT_NO_CONTEXT_ARGS`.
  */
 #define GC_FORMAT_NO_CONTEXT_STRING_LITERAL TEXT("[%s]")
 
 /**
- * Variadic arguments which corresponds with GC_FORMAT_NO_CONTEXT_STRING_LITERAL.
+ * @brief Variadic arguments used to output info about the current context.
+ * @note Arguments correspond with `GC_FORMAT_NO_CONTEXT_STRING_LITERAL`.
  */
 #define GC_FORMAT_NO_CONTEXT_ARGS StringCast<TCHAR, 512>(std::source_location::current().function_name()).Get()
 
 /**
- * If you can't get ahold of a UObject, you may need to use this.
+ * @brief Log macro with no additional context to output out extra info for.
  */
 #define GC_LOG_NO_CONTEXT(categoryName, verbosity, format, ...) UE_LOG(categoryName, verbosity, GC_FORMAT_NO_CONTEXT_STRING_LITERAL TEXT(" ") format, GC_FORMAT_NO_CONTEXT_ARGS __VA_OPT__(,) __VA_ARGS__)
 
 /**
- * Conditional log version of GC_LOG_NO_CONTEXT().
+ * @brief Conditional log version of `GC_LOG_NO_CONTEXT()` macro.
  */
 #define GC_CLOG_NO_CONTEXT(condition, categoryName, verbosity, format, ...) UE_CLOG(condition, categoryName, verbosity, GC_FORMAT_NO_CONTEXT_STRING_LITERAL TEXT(" ") format, GC_FORMAT_NO_CONTEXT_ARGS __VA_OPT__(,) __VA_ARGS__)
 
 /**
- * Format string for logging output about a context object.
+ * @brief Format string literal used in UObject-context log macros. To output info about the context.
  */
-#define GC_FORMAT_CONTEXT_OBJECT_STRING_LITERAL TEXT("[Context Object Arg: `%s`]")
+#define GC_FORMAT_UOBJECT_STRING_LITERAL TEXT("[Context Object Arg: `%s`]")
 
 /**
- * @param contextObject UObject which will be used when our log gathers extra data to help us out. May be a UWorld.
+ * @brief Log macro with a `UObject` for context to output out extra info.
+ * @param contextObject The `UObject*` used to output out extra info. May be a `UWorld`.
  */
-#define GC_LOG(contextObject, categoryName, verbosity, format, ...) GC_LOG_NO_CONTEXT(categoryName, verbosity, format TEXT(" ") GC_FORMAT_CONTEXT_OBJECT_STRING_LITERAL TEXT(" %s"), __VA_ARGS__ __VA_OPT__(,) TEXT(PREPROCESSOR_TO_STRING(contextObject)), GCUtils::Log::GetObjectLogInfoString(contextObject).ToString())
+#define GC_LOG_UOBJECT(contextObject, categoryName, verbosity, format, ...) GC_LOG_NO_CONTEXT(categoryName, verbosity, format TEXT(" ") GC_FORMAT_UOBJECT_STRING_LITERAL TEXT(" %s"), __VA_ARGS__ __VA_OPT__(,) TEXT(PREPROCESSOR_TO_STRING(contextObject)), GCUtils::Log::GetObjectLogInfoString(contextObject).ToString())
 
  /**
-  * Conditional log version of GC_LOG().
+  * @brief Conditional log version of `GC_LOG_UOBJECT()` macro.
   */
-#define GC_CLOG(contextObject, condition, categoryName, verbosity, format, ...) GC_CLOG_NO_CONTEXT(condition, categoryName, verbosity, format TEXT(" ") GC_FORMAT_CONTEXT_OBJECT_STRING_LITERAL TEXT(" %s"), __VA_ARGS__ __VA_OPT__(,) TEXT(PREPROCESSOR_TO_STRING(contextObject)), GCUtils::Log::GetObjectLogInfoString(contextObject).ToString())
+#define GC_CLOG_UOBJECT(contextObject, condition, categoryName, verbosity, format, ...) GC_CLOG_NO_CONTEXT(condition, categoryName, verbosity, format TEXT(" ") GC_FORMAT_UOBJECT_STRING_LITERAL TEXT(" %s"), __VA_ARGS__ __VA_OPT__(,) TEXT(PREPROCESSOR_TO_STRING(contextObject)), GCUtils::Log::GetObjectLogInfoString(contextObject).ToString())
 
 namespace GCUtils::Log
 {
     /**
-     * Builds a debug string given a UObject.
+     * @brief Build a debug string given a `UObject`.
      */
     template <int32 bufferLength = 1024>
     TStringBuilder<bufferLength> GetObjectLogInfoString(const UObject* object);
