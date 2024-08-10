ecda59ef3ddfaede5e2311c58e0f9cc9728fa520
Christian Hinkle <christianhinko@gmail.com>
2024-08-10 07:29:53 -0400

    [Utils] [ScriptStruct] Improve concepts/constraints/requires code and documentation.
    Fully use constraints instead of requires clauses for functions now. Make appropriate concepts to achieve this. Makes code easier to understand and smaller.
    
    Clean up a lot of leftover mess.

--- Source/GameCore/Public/GCUtils_ScriptStruct.h
+++ Source/GameCore/Public/GCUtils_ScriptStruct.h
@@ -5,68 +5,143 @@
 #include "CoreMinimal.h"
 #include "type_traits"
 #include "GCConcepts.h"
-#include "Templates/GCHasStaticScriptStruct.h"
+#include "Templates/Models.h"
+#include "Concepts/StaticStructProvider.h"
 #include "Concepts/BaseStructureProvider.h"
-#include "Templates/GCHasGetScriptStruct.h"
 
 /**
- *
+ * @brief Utilities for working with USTRUCTs and intrinsic base structures (like `FSoftObjectPath`).
  */
 namespace GCUtils::ScriptStruct
 {
     /**
-     * @brief Dynamic cast function for USTRUCTs.
-     * @param inValue Pointer to the struct object.
-     * @return Casted pointer to the struct object. Null if script structs are incompatible.
-     */
-    template <GCConcepts::Pointer TTo, GCConcepts::Pointer TFrom>
-        requires (GCHasStaticScriptStruct<std::remove_pointer_t<TTo>>() && GCHasGetScriptStruct<std::remove_pointer_t<TFrom>>())
-    FORCEINLINE_DEBUGGABLE TTo CastScriptStruct(TFrom inValue);
-
-    /**
-     * @brief Dynamic cast function for USTRUCTs.
-     * @param inValue Reference to the struct object.
-     * @return Casted pointer to the struct object. Null if script structs are incompatible.
-     */
-    template <GCConcepts::Pointer TTo, GCConcepts::Reference TFrom>
-        requires (GCHasStaticScriptStruct<std::remove_pointer_t<TTo>>() && GCHasGetScriptStruct<std::remove_reference_t<TFrom>>())
-    FORCEINLINE_DEBUGGABLE TTo CastScriptStruct(TFrom&& inValue);
-
-    /**
-     * @brief Dynamic cast function for USTRUCTs. Check-asserts that script structs are compatible.
-     * @param inValue Pointer to the struct object.
-     * @return Casted reference to the struct object.
-     */
-    template <GCConcepts::Reference TTo, GCConcepts::Pointer TFrom>
-        requires (GCHasStaticScriptStruct<std::remove_reference_t<TTo>>() && GCHasGetScriptStruct<std::remove_pointer_t<TFrom>>())
-    FORCEINLINE_DEBUGGABLE TTo CastScriptStruct(TFrom inValue);
-
-    /**
-     * @brief Dynamic cast function for USTRUCTs. Check-asserts that script structs are compatible.
-     * @param inValue Reference to the struct object.
-     * @return Casted reference to the struct object.
-     */
-    template <GCConcepts::Reference TTo, GCConcepts::Reference TFrom>
-        requires (GCHasStaticScriptStruct<std::remove_reference_t<TTo>>() && GCHasGetScriptStruct<std::remove_reference_t<TFrom>>())
-    FORCEINLINE_DEBUGGABLE TTo CastScriptStruct(TFrom&& inValue);
-
-    /**
-     * @brief Get the `UScriptStruct` for a struct type.
-     * @note Works for both USTRUCTs and instrinsic base structures, e.g., `FSoftObjectPath`.
+     * @brief Test if a given type a static getter function for the `UScriptStruct`.
+     * @note This is generic to all USTRUCTs as well as intrinsic base structures (like `FSoftObjectPath`).
      */
     template <class T>
-    UScriptStruct* GCGetStaticScriptStruct();
+    consteval bool HasStaticScriptStructGetter();
+
+    /**
+     * @brief Test if a given type has the `UScriptStruct* GetScriptStruct() const` member function.
+     */
+    template <class T>
+    consteval bool HasRuntimeScriptStructGetter();
+
+    /**
+     * @brief Concept satisfied by types with static getter functions to their script structs.
+     * @note This is generic to all USTRUCTs as well as intrinsic base structures (like `FSoftObjectPath`).
+     */
+    template <class T>
+    concept GettableStaticScriptStruct = HasStaticScriptStructGetter<T>();
+
+    /**
+     * @brief Concept satisfied by types with non-static getter functions to their script structs.
+     */
+    template <class T>
+    concept GettableRuntimeScriptStruct = HasRuntimeScriptStructGetter<T>();
+
+    template <class T>
+    concept PointerToGettableStaticScriptStruct = GCConcepts::Pointer<T> && GettableStaticScriptStruct<std::remove_pointer_t<T>>;
+
+    template <class T>
+    concept PointerToGettableRuntimeScriptStruct = GCConcepts::Pointer<T> && GettableRuntimeScriptStruct<std::remove_pointer_t<T>>;
+
+    template <class T>
+    concept ReferenceToGettableStaticScriptStruct = GCConcepts::Reference<T> && GettableStaticScriptStruct<std::remove_reference_t<T>>;
+
+    template <class T>
+    concept ReferenceToGettableRuntimeScriptStruct = GCConcepts::Reference<T> && GettableRuntimeScriptStruct<std::remove_reference_t<T>>;
+
+    /**
+     * @brief Dynamic cast function for USTRUCTs.
+     * @param inValue Pointer to the struct object.
+     * @return Casted pointer to the struct object. Null if script structs are incompatible.
+     */
+    template <PointerToGettableStaticScriptStruct TTo, PointerToGettableRuntimeScriptStruct TFrom>
+    FORCEINLINE_DEBUGGABLE TTo CastScriptStruct(TFrom inValue);
+
+    /**
+     * @brief Dynamic cast function for USTRUCTs.
+     * @param inValue Reference to the struct object.
+     * @return Casted pointer to the struct object. Null if script structs are incompatible.
+     */
+    template <PointerToGettableStaticScriptStruct TTo, ReferenceToGettableRuntimeScriptStruct TFrom>
+    FORCEINLINE_DEBUGGABLE TTo CastScriptStruct(TFrom&& inValue);
+
+    /**
+     * @brief Dynamic cast function for USTRUCTs. Check-asserts that script structs are compatible.
+     * @param inValue Pointer to the struct object.
+     * @return Casted reference to the struct object.
+     */
+    template <ReferenceToGettableStaticScriptStruct TTo, PointerToGettableRuntimeScriptStruct TFrom>
+    FORCEINLINE_DEBUGGABLE TTo CastScriptStruct(TFrom inValue);
+
+    /**
+     * @brief Dynamic cast function for USTRUCTs. Check-asserts that script structs are compatible.
+     * @param inValue Reference to the struct object.
+     * @return Casted reference to the struct object.
+     */
+    template <ReferenceToGettableStaticScriptStruct TTo, ReferenceToGettableRuntimeScriptStruct TFrom>
+    FORCEINLINE_DEBUGGABLE TTo CastScriptStruct(TFrom&& inValue);
 
     /**
      * @brief Get the `UScriptStruct` for a struct type.
-     * @note Works for both USTRUCTs and instrinsic base structures, e.g., `FSoftObjectPath`.
+     * @note This is generic to all USTRUCTs as well as intrinsic base structures (like `FSoftObjectPath`).
      */
-    template <class T, class = std::enable_if_t<GCHasGetScriptStruct<T>()>>
-    UScriptStruct& GCGetStaticScriptStructChecked();
+    template <GettableStaticScriptStruct T>
+    UScriptStruct* GetScriptStructStatic();
+
+    /**
+     * @brief Get the `UScriptStruct` for a struct type.
+     * @note This is generic to all USTRUCTs as well as intrinsic base structures (like `FSoftObjectPath`).
+     */
+    template <GettableStaticScriptStruct T>
+    UScriptStruct& GetScriptStructStaticChecked();
 }
 
-template <GCConcepts::Pointer TTo, GCConcepts::Pointer TFrom>
-    requires (GCHasStaticScriptStruct<std::remove_pointer_t<TTo>>() && GCHasGetScriptStruct<std::remove_pointer_t<TFrom>>())
+template <class T>
+consteval bool GCUtils::ScriptStruct::HasStaticScriptStructGetter()
+{
+    if constexpr (TModels<CStaticStructProvider, T>::Value)
+    {
+        return true;
+    }
+
+    if constexpr (TModels<CBaseStructureProvider, T>::Value)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+template <class T>
+consteval bool GCUtils::ScriptStruct::HasRuntimeScriptStructGetter()
+{
+    using FRequiredMemberFunctionPtrType = UScriptStruct* (T::*)() const;
+
+    constexpr bool hasMember =
+        requires { static_cast<FRequiredMemberFunctionPtrType>(&T::GetScriptStruct); };
+
+    if constexpr (!hasMember)
+    {
+        // Neither `T` nor any of its parent classes have the GetScriptStruct function.
+        return false;
+    }
+
+    using FExistingMemberFunctionPtrType = decltype(&T::GetScriptStruct);
+    constexpr bool hasMemberExact = std::same_as<FExistingMemberFunctionPtrType, FRequiredMemberFunctionPtrType>;
+
+    if constexpr (!hasMemberExact)
+    {
+        // `T` itself doesn't have the GetScriptStruct function.
+        return false;
+    }
+
+    return true;
+}
+
+template <GCUtils::ScriptStruct::PointerToGettableStaticScriptStruct TTo, GCUtils::ScriptStruct::PointerToGettableRuntimeScriptStruct TFrom>
 TTo GCUtils::ScriptStruct::CastScriptStruct(TFrom inValue)
 {
     using FToStruct = std::remove_pointer_t<TTo>;
@@ -87,8 +162,7 @@ TTo GCUtils::ScriptStruct::CastScriptStruct(TFrom inValue)
     return CastScriptStruct<TTo>(*inValue);
 }
 
-template <GCConcepts::Pointer TTo, GCConcepts::Reference TFrom>
-    requires (GCHasStaticScriptStruct<std::remove_pointer_t<TTo>>() && GCHasGetScriptStruct<std::remove_reference_t<TFrom>>())
+template <GCUtils::ScriptStruct::PointerToGettableStaticScriptStruct TTo, GCUtils::ScriptStruct::ReferenceToGettableRuntimeScriptStruct TFrom>
 TTo GCUtils::ScriptStruct::CastScriptStruct(TFrom&& inValue)
 {
     using FToStruct = std::remove_pointer_t<TTo>;
@@ -107,7 +181,7 @@ TTo GCUtils::ScriptStruct::CastScriptStruct(TFrom&& inValue)
         return nullptr;
     }
 
-    const UScriptStruct& toScriptStruct = GCGetStaticScriptStructChecked<FToStruct>();
+    const UScriptStruct& toScriptStruct = GetScriptStructStaticChecked<FToStruct>();
     if (toScriptStruct.IsChildOf(fromScriptStruct) == false)
     {
         return nullptr;
@@ -116,8 +190,7 @@ TTo GCUtils::ScriptStruct::CastScriptStruct(TFrom&& inValue)
     return &static_cast<FToStruct&>(Forward<TFrom>(inValue));
 }
 
-template <GCConcepts::Reference TTo, GCConcepts::Pointer TFrom>
-    requires (GCHasStaticScriptStruct<std::remove_reference_t<TTo>>() && GCHasGetScriptStruct<std::remove_pointer_t<TFrom>>())
+template <GCUtils::ScriptStruct::ReferenceToGettableStaticScriptStruct TTo, GCUtils::ScriptStruct::PointerToGettableRuntimeScriptStruct TFrom>
 TTo GCUtils::ScriptStruct::CastScriptStruct(TFrom inValue)
 {
     using FToStruct = std::remove_reference_t<TTo>;
@@ -135,8 +208,7 @@ TTo GCUtils::ScriptStruct::CastScriptStruct(TFrom inValue)
     return CastScriptStruct<TTo>(*inValue);
 }
 
-template <GCConcepts::Reference TTo, GCConcepts::Reference TFrom>
-    requires (GCHasStaticScriptStruct<std::remove_reference_t<TTo>>() && GCHasGetScriptStruct<std::remove_reference_t<TFrom>>())
+template <GCUtils::ScriptStruct::ReferenceToGettableStaticScriptStruct TTo, GCUtils::ScriptStruct::ReferenceToGettableRuntimeScriptStruct TFrom>
 TTo GCUtils::ScriptStruct::CastScriptStruct(TFrom&& inValue)
 {
     using FToStruct = std::remove_reference_t<TTo>;
@@ -152,14 +224,14 @@ TTo GCUtils::ScriptStruct::CastScriptStruct(TFrom&& inValue)
     UScriptStruct* fromScriptStruct = inValue.GetScriptStruct();
     check(fromScriptStruct);
 
-    const UScriptStruct& toScriptStruct = GCGetStaticScriptStructChecked<std::remove_reference_t<TTo>>();
+    const UScriptStruct& toScriptStruct = GetScriptStructStaticChecked<FToStruct>();
     check(toScriptStruct.IsChildOf(fromScriptStruct));
 
     return static_cast<TTo>(Forward<TFrom>(inValue));
 }
 
-template <class T>
-UScriptStruct* GCUtils::ScriptStruct::GCGetStaticScriptStruct()
+template <GCUtils::ScriptStruct::GettableStaticScriptStruct T>
+UScriptStruct* GCUtils::ScriptStruct::GetScriptStructStatic()
 {
     if constexpr (TModels<CStaticStructProvider, T>::Value)
     {
@@ -174,9 +246,9 @@ UScriptStruct* GCUtils::ScriptStruct::GCGetStaticScriptStruct()
     return nullptr;
 }
 
-template <class T, class>
-UScriptStruct& GCUtils::ScriptStruct::GCGetStaticScriptStructChecked()
+template <GCUtils::ScriptStruct::GettableStaticScriptStruct T>
+UScriptStruct& GCUtils::ScriptStruct::GetScriptStructStaticChecked()
 {
-    check(GCGetStaticScriptStruct<T>());
-    return *GCGetStaticScriptStruct<T>();
+    check(GetScriptStructStatic<T>());
+    return *GetScriptStructStatic<T>();
 }
