8168d931f0e304f337d8127aaffe620843992f3b
Christian Hinkle <christianhinko@gmail.com>
2024-10-11 19:39:04 -0600

    [Log] Make "IInterface" versions of contextual log macros.
    Also, delete an incorrect "Internal macro!" comment.

--- Source/GameCore/Public/GCUtils_Log.h
+++ Source/GameCore/Public/GCUtils_Log.h
@@ -93,7 +93,6 @@
 
 /**
  * @brief Conditional log with a `UObject` for context to output extra info for. Printf style.
- * @note Internal macro! Not intended for use.
  * @param contextObject The `UObject*` used to output extra info.
  */
 #define GC_CLOG_FMT_UOBJECT(contextObject, condition, categoryName, verbosity, format, ...) GC_CLOG_FMT_NO_CONTEXT(condition, categoryName, verbosity, format TEXT(" ") GC_PRIVATE_INFO_UOBJECT_FORMAT_STRING_LITERAL TEXT(" %s"), __VA_ARGS__ __VA_OPT__(,) TEXT(PREPROCESSOR_TO_STRING(contextObject)), GCUtils::Log::GetObjectLogInfoString(contextObject).ToString())
@@ -104,6 +103,26 @@
  * @param string String to output. Overloads of functions `::GetNum()` and `::GetData()` must exist for it.
  */
 #define GC_CLOG_STR_UOBJECT(contextObject, condition, categoryName, verbosity, string) { const auto& refBoundString = string; GC_CLOG_FMT_UOBJECT(contextObject, condition, categoryName, verbosity, TEXT("%.*s"), GetNum(refBoundString), GetData(refBoundString)); }
+
+/**
+ * @brief Version of `GC_LOG_FMT_UOBJECT()` for `IInterface`s.
+ */
+#define GC_LOG_FMT_IINTERFACE(contextInterface, categoryName, verbosity, format, ...) GC_LOG_FMT_UOBJECT(contextInterface->_getUObject(), categoryName, verbosity, format, ...)
+
+/**
+ * @brief Version of `GC_LOG_STR_UOBJECT()` for `IInterface`s.
+ */
+#define GC_LOG_STR_IINTERFACE(contextInterface, categoryName, verbosity, string) GC_LOG_STR_UOBJECT(contextInterface->_getUObject(), categoryName, verbosity, string)
+
+/**
+ * @brief Version of `GC_CLOG_FMT_UOBJECT()` for `IInterface`s.
+ */
+#define GC_CLOG_FMT_IINTERFACE(contextInterface, condition, categoryName, verbosity, format, ...) GC_CLOG_FMT_UOBJECT(contextInterface->_getUObject(), condition, categoryName, verbosity, format, ...)
+
+/**
+ * @brief Version of `GC_CLOG_STR_UOBJECT()` for `IInterface`s.
+ */
+#define GC_CLOG_STR_IINTERFACE(contextInterface, condition, categoryName, verbosity, string) GC_CLOG_STR_UOBJECT(contextInterface->_getUObject(), condition, categoryName, verbosity, string)
 
 namespace GCUtils::Log
 {
