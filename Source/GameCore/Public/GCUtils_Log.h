53d7260167dcf1d734cf0cb9fe097008ca59769e
Christian Hinkle <christianhinko@gmail.com>
2024-10-11 19:42:18 -0600

    [Log] Add global scope resolution to macro code. Helps avoid conflicts with user's code.

--- Source/GameCore/Public/GCUtils_Log.h
+++ Source/GameCore/Public/GCUtils_Log.h
@@ -19,7 +19,7 @@
  * @note Prefer the contextual log macros over this one.
  * @param string String to output. Overloads of functions `::GetNum()` and `::GetData()` must exist for it.
  */
-#define GC_PLAIN_LOG_STR(categoryName, verbosity, string) { const auto& refBoundString = string; GC_PLAIN_LOG_FMT(categoryName, verbosity, TEXT("%.*s"), GetNum(refBoundString), GetData(refBoundString)); }
+#define GC_PLAIN_LOG_STR(categoryName, verbosity, string) { const auto& refBoundString = string; GC_PLAIN_LOG_FMT(categoryName, verbosity, TEXT("%.*s"), ::GetNum(refBoundString), ::GetData(refBoundString)); }
 
 /**
  * @brief Plain conditional log. Printf style.
@@ -34,7 +34,7 @@
  * @note Prefer the contextual log macros over this one.
  * @param string String to output. Overloads of functions `::GetNum()` and `::GetData()` must exist for it.
  */
-#define GC_PLAIN_CLOG_STR(condition, categoryName, verbosity, string) { const auto& refBoundString = string; GC_PLAIN_CLOG_FMT(condition, categoryName, verbosity, TEXT("%.*s"), GetNum(refBoundString), GetData(refBoundString)); }
+#define GC_PLAIN_CLOG_STR(condition, categoryName, verbosity, string) { const auto& refBoundString = string; GC_PLAIN_CLOG_FMT(condition, categoryName, verbosity, TEXT("%.*s"), ::GetNum(refBoundString), ::GetData(refBoundString)); }
 
 /**
  * @brief Extra format string literal used in no-context logs. For outputting info about the current context.
@@ -59,7 +59,7 @@
  * @brief Log with no extra context to output info for. From string.
  * @param string String to output. Overloads of functions `::GetNum()` and `::GetData()` must exist for it.
  */
-#define GC_LOG_STR_NO_CONTEXT(categoryName, verbosity, string) { const auto& refBoundString = string; GC_LOG_FMT_NO_CONTEXT(categoryName, verbosity, TEXT("%.*s"), GetNum(refBoundString), GetData(refBoundString)); }
+#define GC_LOG_STR_NO_CONTEXT(categoryName, verbosity, string) { const auto& refBoundString = string; GC_LOG_FMT_NO_CONTEXT(categoryName, verbosity, TEXT("%.*s"), ::GetNum(refBoundString), ::GetData(refBoundString)); }
 
 /**
  * @brief Conditional log with no extra context to output info for. Printf style.
@@ -70,7 +70,7 @@
  * @brief Conditional log with no extra context to output info for. From string.
  * @param string String to output. Overloads of functions `::GetNum()` and `::GetData()` must exist for it.
  */
-#define GC_CLOG_STR_NO_CONTEXT(condition, categoryName, verbosity, string) { const auto& refBoundString = string; GC_CLOG_FMT_NO_CONTEXT(condition, categoryName, verbosity, TEXT("%.*s"), GetNum(refBoundString), GetData(refBoundString)); }
+#define GC_CLOG_STR_NO_CONTEXT(condition, categoryName, verbosity, string) { const auto& refBoundString = string; GC_CLOG_FMT_NO_CONTEXT(condition, categoryName, verbosity, TEXT("%.*s"), ::GetNum(refBoundString), ::GetData(refBoundString)); }
 
 /**
  * @brief Extra format string literal used in UObject-context logs. For outputting info about the current context.
@@ -89,7 +89,7 @@
  * @param contextObject The `UObject*` used to output extra info.
  * @param string String to output. Overloads of functions `::GetNum()` and `::GetData()` must exist for it.
  */
-#define GC_LOG_STR_UOBJECT(contextObject, categoryName, verbosity, string) { const auto& refBoundString = string; GC_LOG_FMT_UOBJECT(contextObject, categoryName, verbosity, TEXT("%.*s"), GetNum(refBoundString), GetData(refBoundString)); }
+#define GC_LOG_STR_UOBJECT(contextObject, categoryName, verbosity, string) { const auto& refBoundString = string; GC_LOG_FMT_UOBJECT(contextObject, categoryName, verbosity, TEXT("%.*s"), ::GetNum(refBoundString), ::GetData(refBoundString)); }
 
 /**
  * @brief Conditional log with a `UObject` for context to output extra info for. Printf style.
@@ -102,7 +102,7 @@
  * @param contextObject The `UObject*` used to output extra info.
  * @param string String to output. Overloads of functions `::GetNum()` and `::GetData()` must exist for it.
  */
-#define GC_CLOG_STR_UOBJECT(contextObject, condition, categoryName, verbosity, string) { const auto& refBoundString = string; GC_CLOG_FMT_UOBJECT(contextObject, condition, categoryName, verbosity, TEXT("%.*s"), GetNum(refBoundString), GetData(refBoundString)); }
+#define GC_CLOG_STR_UOBJECT(contextObject, condition, categoryName, verbosity, string) { const auto& refBoundString = string; GC_CLOG_FMT_UOBJECT(contextObject, condition, categoryName, verbosity, TEXT("%.*s"), ::GetNum(refBoundString), ::GetData(refBoundString)); }
 
 /**
  * @brief Version of `GC_LOG_FMT_UOBJECT()` for `IInterface`s.
